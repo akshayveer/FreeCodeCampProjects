483816766ab8ad241905e9142ce2a57033d98da2
akshayveer <akshayveer096@gmail.com>
2017-06-21 19:55:51 +0530

    Add weather icons cdn Create mapping between we weather api icons names and actual weather icons from icon library

--- display-local-wether/main.js
+++ display-local-wether/main.js
@@ -1,3 +1,35 @@
+// weatehr api cdn https://cdnjs.com/libraries/weather-icons
+var weather_icons_name_mapping = {
+  "clear-day" : "wi-day-sunny",
+  "clear-night" : "wi-night-clear",
+  "rain-day" : "wi-day-rain",
+  "rain-night" : "wi-night-alt-rain-wind",
+  "snow-day" : "wi-day-snow",
+  "snow-night" : "wi-night-alt-snow",
+  "sleet-day" : "wi-day-sleet",
+  "sleet-night" : "wi-night-sleet",
+  "wind-day" : "wi-day-windy",
+  "wind-night" : "wi-night-cloudy-windy",
+  "fog-day" : "wi-day-fog",
+  "fog-night" : "wi-night-fog",
+  "cloudy-day" : "wi-day-cloudy",
+  "cloudy-night" : "wi-night-cloudy",
+  "partly-cloudy-day-day" : "wi-day-cloudy-gusts",
+  "partly-cloudy-night-night" : "wi-night-partly-cloudy",
+  "hail-day" : "wi-day-hail",
+  "hail-night" : "wi-night-hail",
+  "thunderstorm-day" : "wi-day-thunderstorm",
+  "thunderstorm-night" : "wi-night-thunderstorm",
+  "default" : "wi-day-sunny"
+}
+
+function check_weather_icons_functionality(){
+  console.log('adding elements');
+  for (key in weather_icons_name_mapping){
+    $('.container').append('<div style="text-size:40px">'+ key + ' <i class="wi ' + weather_icons_name_mapping[key] + '"</i></div>');
+  }
+}
+
 function get_location_suceess(position) {
   latitude = position.coords.latitude;
   longitute = position.coords.longitude;
@@ -41,7 +73,7 @@ function get_weather_forecast(latitude, longitute) {
 
 if ("geolocation" in navigator) {
   /* geolocation is available */
-  navigator.geolocation.getCurrentPosition(get_location_suceess, get_location_failure);
+  //navigator.geolocation.getCurrentPosition(get_location_suceess, get_location_failure);
 } else {
   alert("Geo loation is not available. Please use this app on different browser which has geolocation");
 }
