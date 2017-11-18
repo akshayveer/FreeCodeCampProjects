29b2b3be33f639d0f225fefee870e9ea6206cba1
akshayveer <akshayveer096@gmail.com>
2017-11-18 19:51:39 +0530

    create Table component and render

---
+++ camper-leaderboard/src/Table.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+  state = { sortBy: 'recent-points' };
+
+  render() {
+    return (
+      <table
+        className="striped responsive-table"
+        style={{ width: '50%', margin: 'auto' }}
+      >
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Camper Name</th>
+            <th>Points in past 30 days</th>
+            <th>All time points</th>
+          </tr>
+        </thead>
+      </table>
+    );
+  }
+}
+
+export default Table;
